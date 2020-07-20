3deb36c5d9a9513f7784c747bc0bd4f809b48ae9
achmadsyafriyal <50198991+achmadsyafriyal@users.noreply.github.com>
2020-07-20 14:07:00 +0700

    menambahkan fungsi

--- IOT/app/src/main/java/com/example/iot/MainActivity.java
+++ IOT/app/src/main/java/com/example/iot/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -13,23 +15,23 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-//inisialisasi text nilai
+    //inisialisasi text nilai
+
     private TextView nilai1;
     private TextView nilai2;
     private TextView nilai3;
     private TextView nilai4;
+    private TextView Status;
 
- //   private TextView Status;
 
 
-    //buat reference untuk firebase (koneksi server / host firebase
+    //buat reference untuk firebase (koneksi server / host firebase)
 
     private Firebase mRef1;
     private Firebase mRef2;
     private Firebase mRef3;
     private Firebase mRef4;
- //  private Firebase mRef5;
-
+    private Firebase mRef5;
 
 
     @Override
@@ -41,17 +43,13 @@ public class MainActivity extends AppCompatActivity {
         nilai2 = (TextView) findViewById (R.id.nilai2);
         nilai3 = (TextView) findViewById (R.id.nilai3);
         nilai4 = (TextView) findViewById (R.id.nilai4);
-    //    Status = (TextView) findViewById (R.id.Status);
-
+        Status = (TextView) findViewById (R.id.Status);
 
         mRef1 = new Firebase ("https://sensoriot-39203.firebaseio.com/api");
         mRef2 = new Firebase ("https://sensoriot-39203.firebaseio.com/smoke1");
         mRef3 = new Firebase ("https://sensoriot-39203.firebaseio.com/smoke2");
         mRef4 = new Firebase ("https://sensoriot-39203.firebaseio.com/suhu");
-//        mRef5 = new Firebase ("https://sensoriot-39203.firebaseio.com/");
-
-
-
+        mRef5 = new Firebase ("https://sensoriot-39203.firebaseio.com/status");
 
         mRef1.addValueEventListener(new ValueEventListener() {
             @Override
@@ -94,17 +92,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    //    mRef5.addValueEventListener(new ValueEventListener() {
-   //         @Override
-      //      public void onDataChange(DataSnapshot dataSnapshot) {
-      //          String status = dataSnapshot.getValue(String.class);
-       //         Status.setText(status);
-     //       }
-     //       @Override
-     //       public void onCancelled(FirebaseError firebaseError) {
-      //      }
-      //  });
+        mRef5.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String status = dataSnapshot.getValue(String.class);
+                Status.setText(status);
+           }
+           @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+       });
 
-
+    }
+    public void panduan (View v) {
+        Toast.makeText(getApplicationContext(), "Mohon maaf, sistem sedang dalam pengembangan.", Toast.LENGTH_LONG).show();
     }
 }
